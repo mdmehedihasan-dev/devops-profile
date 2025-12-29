fd192f7a3130bc3c4335cd72578be60e757e77ae
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-12-29 22:42:37 +0600

    date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
-// Footer.jsx
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-200 py-4 text-center">
-      <p>© 2024 Nure Siddiq - All Rights Reserved</p>
+      <p>© 2025 MD Mehedi Hasan - All Rights Reserved</p>
     </footer>
   );
 }
